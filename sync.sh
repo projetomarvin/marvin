0432302b1b3b067c9675f76cc833f1c09470f786
Dante Nolasco <dnolascodante@gmail.com>
2019-02-27 17:34:54 -0300

    make connect url variable

--- sync.sh
+++ sync.sh
@@ -1,4 +1,2 @@
-sed -i 's/localhost/api.projetomarvin.com/g' *.html
 aws s3 sync . s3://app.projetomarvin.com --acl public-read --exclude ".git*" --exclude ".gitignore*" --profile cori
 aws cloudfront create-invalidation --distribution-id E3VBOWG22MUCR0 --paths /* --profile cori
-sed -i 's/api.projetomarvin.com/localhost/g' *.html
