f5d7bc9a1b964098c618bc2591c60a767823a644
Dante Nolasco <dnolascodante@gmail.com>
2019-12-02 13:32:40 -0300

    fix initial code reading

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
-aws s3 sync . s3://app.projetomarvin.com --acl public-read --exclude ".git*" --exclude ".gitignore*" --profile cori
-aws cloudfront create-invalidation --distribution-id E3VBOWG22MUCR0 --paths /* --profile cori
+aws s3 sync . s3://app.projetomarvin.com --acl public-read --exclude ".git*" --exclude ".gitignore*"
+aws cloudfront create-invalidation --distribution-id E3VBOWG22MUCR0 --paths /*
